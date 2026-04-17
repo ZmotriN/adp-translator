16063f82199eed3077ca56d0c91bad82ecd39973
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-04-16 22:41:04 -0400

    Favicon

--- dist/styles/adp-translator.core.min.css
+++ dist/styles/adp-translator.core.min.css
@@ -1,6 +1,6 @@
 /*!
 
-    Généré: Jeudi le 16 avril 2026 à 22 h 33
+    Généré: Jeudi le 16 avril 2026 à 22 h 40
     Auteur: Maxime Larrivée-Roy <mlarriveeroy@gmail.com>
     Github: https://github.com/ZmotriN/adp-visualiser
 
